d1eff6899bcfc6231105d328af38ca6fecf18b8e
nazril <dnazrilsanz@gmail.com>
2026-05-23 15:29:31 +0700

    belum disave hehe

--- easy/checkIfArrayIsSorted.js
+++ easy/checkIfArrayIsSorted.js
@@ -3,21 +3,16 @@
  * @return {boolean}
  */
 var check = function(nums) {
-    // let count = 0;
-    // let n = nums.length;
-
-    // for (let i = 0; i < n; i++) {
-    //     if (nums[i] > nums[(i + 1) % n]) {
-    //         count++;
-    //     }
-    // }
-
-    // return count <= 1;
+    let count = 0;
     let n = nums.length;
-    for (let i = 0; i < nums.length; i++) {
-        console.log(nums[(i + 1) % n]);
+
+    for (let i = 0; i < n; i++) {
+        if (nums[i] > nums[(i + 1) % n]) {
+            count++;
+        }
     }
 
+    return count <= 1;
 };
 
 const nums = [2,2,3,4,5,2];
